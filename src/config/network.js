5e6c45356b8b9506dee92676ecd6b805dbb0bb1d
Kashif130 <54382276+Kashif130@users.noreply.github.com>
2026-08-20 07:29:51 +0500

    Update network.js

--- src/config/network.js
+++ src/config/network.js
@@ -5,13 +5,13 @@ dotenv.config();
 export const bradburyNetwork = {
     chainIdHex: '0x107D',
     chainName: 'GenLayer Bradbury',
-    rpcUrls: ('https://zksync-os-testnet-genlayer.zksync.dev').split(','),
+    rpcUrls: ('https://rpc.testnet-chain.genlayer.com').split(','),
     nativeCurrency: {
         name: process.env.NATIVE_CURRENCY_NAME || 'GEN',
         symbol: process.env.NATIVE_CURRENCY_SYMBOL || 'GEN',
         decimals: 18,
     },
-    blockExplorerUrls: ('http://explorer-bradbury.genlayer.com/,https://zksync-os-testnet-genlayer.explorer.zksync.dev/').split(','),
+    blockExplorerUrls: ('https://explorer.testnet-chain.genlayer.com').split(','),
 };
 
 export const studionetNetwork = {
